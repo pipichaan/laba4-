445f963773eeb06e54eb40da4308affbe14602e6
pipichaan <147584671+pipichaan@users.noreply.github.com>
2023-10-24 12:19:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+sum(n);
+void sum(int c)
+{
+    int s = 0;
+    for (int i = 0; i <= n; i += 2)
+    {
+        s = s + i;
+
+    }
+    Console.WriteLine(s);
+}
+proiz(n);
+
+void proiz(int d)
+        {
+            int p = 1;
+            for (int i = 1; i <= n; i += 2)
+            {
+                p = p * i;
+            }
+            Console.WriteLine(p);
+        }
